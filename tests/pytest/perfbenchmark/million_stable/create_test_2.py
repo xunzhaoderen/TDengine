ec30dcf7e62eb5df72ce6c21d349037560d3b2f8
bryanchang0603 <changb9@mcamster.ca>
2021-06-22 17:55:57 +0800

    update insert_test.py

--- tests/pytest/perfbenchmark/million_stable/create_test_2.py
+++ tests/pytest/perfbenchmark/million_stable/create_test_2.py
@@ -31,16 +31,16 @@ class TDTestCase:
         tdLog.debug("start to execute %s" % __file__)
         tdSql.init(conn.cursor(), logSql)
         self.stableLimit = 500
+        self.stbOffset = 1000000/2
 
-    def stable_insert(self, startNum, insertTemplate, localTaosdemoConfig):
+    def stable_creation(self, startNum, insertTemplate, localTaosdemoConfig):
         selfTemplate = insertTemplate
 
-        selfTemplate['name'] = f'stb{startNum}'
+        selfTemplate['name'] = f'stb{startNum + self.stbOffset}'
         selfTemplate['childtable_count'] = 10
-        selfTemplate['child_table_exists'] = 'yes'
         selfTemplate['childtable_prefix'] = f'stb{startNum}_'
         selfTemplate['batch_create_tbl_num'] = 25
-        selfTemplate['insert_rows'] = 200
+        selfTemplate['insert_rows'] = 0
         selfTemplate['columns'] = [{"type": "INT", "count": 2}, {
             "type": "DOUBLE", "count": 2}, {"type": "BINARY", "len": 32, "count": 1}]
         selfTemplate['tags'] = [{"type": "INT", "count": 2}, {
@@ -54,24 +54,21 @@ class TDTestCase:
         localTaosdemoConfig.alter_insert_cfg('host', IP)
         stbTemplate = dict(localTaosdemoConfig.get_template("insert_stbs"))
         for i in range(stbNum):
-            self.stable_insert(int(i + 500 * FileIndex),dict(stbTemplate), localTaosdemoConfig)
+            self.stable_creation(int(i + FileIndex),dict(stbTemplate), localTaosdemoConfig)
         localTaosdemoConfig.generate_insert_cfg(
-            'perfbenchmark/million_stable/temp', f'{stbNum}_stb_{FileIndex}_a')
-        runPath = f'perfbenchmark/million_stable/temp/insert_{stbNum}_stb_{FileIndex}_a.json'
+            'perfbenchmark/million_stable/temp', f'{stbNum}_stb_{FileIndex}')
+        runPath = f'perfbenchmark/million_stable/temp/insert_{stbNum}_stb_{FileIndex}.json'
         print(f'{runPath} created')
         return runPath
 
     def creationThread(self, fileNum, binPath, threadIndex, IP):
         jsonFile = []
         for i in range(fileNum):
-            generatedFile = self.createSingleFile(500, (i + threadIndex * fileNum) * self.stableLimit, IP)
+            generatedFile = self.createSingleFile(500, (i + threadIndex * fileNum ) * self.stableLimit + self.stbOffset, IP)
             jsonFile.append(generatedFile)
         
         # for i in jsonFile:
-        #     try:
-        #         os.system(f"{binPath}taosdemo -f {i} > 1 > /dev/null")
-        #     except BaseException:
-        #         pass
+        #     os.system(f"{binPath}taosdemo -f {i} > 1 > /dev/null")
 
     def run(self):
         tdDnodes.stopAll()
@@ -79,17 +76,20 @@ class TDTestCase:
         IP1 = '192.168.1.86'
         IP2 = '192.168.1.180'
 
-
-
+        # connTaos = taos.connect(host=IP1, user="root", password="taosdata", config="/etc/taos")
+        # c1 = connTaos.cursor()
+        # c1.execute('drop database if exists db')
+        # c1.execute('create database db replica 2')
+        # c1.close()
+        # connTaos.close()
         binPath = tdFindPath.getTaosdemoPath()
 
         threadlist = []
         for i in range(8):
-            threadlist.append(threading.Thread(target = self.creationThread, args = (250,binPath,i,IP1,)))
+            threadlist.append(threading.Thread(target = self.creationThread, args = (2,binPath,i,IP1,)))
             threadlist[i].start()
         for i in range(8):
             threadlist[i].join()
-
 
     def stop(self):
         tdSql.close()
